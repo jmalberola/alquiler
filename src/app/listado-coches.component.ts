93a1cca3e8ba3da9f45274de74342cdc1f0473dc
Juanmi <jmalberola@florida-uni.es>
2017-03-11 15:50:34 +0100

    Proyecto inicial

---
+++ src/app/listado-coches.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'listado-coches',
+  templateUrl: 'app/listado-coches.component.html'
+})
+
+export class ListadoCochesComponent {
+
+
+
+}
